a877f4c42e79b5402284d61af646b2528682b336
jxrvll <jxrvll@gmail.com>
2020-06-29 11:26:04 -0400

    Working weather dashboard

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
 
+// Set global variables
 var city = "";
 
-var cityStorage = JSON.parse(localStorage.getItem("cityStorage"));
+
+var cityStorage = JSON.parse(localStorage.getItem("cityStorage")); 
 if(cityStorage === null){
     cityStorage = [];
 } else{
@@ -20,6 +22,21 @@ $('#search-button').on("click", function(){
     callAPI(city);
 })
 
+$('#clear-search').on("click", function(e){
+    e.preventDefault();
+    localStorage.clear();
+})
+
+function outputDate(x){
+    var targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + x);
+    console.log(targetDate);
+    var month = targetDate.getMonth() + 1;
+    var day = targetDate.getDate();
+    var year = targetDate.getFullYear();
+    return month + "/" + day + "/" + year;
+}
+
 function callAPI(x){
     // Open weather API call
     city = x;
@@ -34,12 +51,48 @@ function callAPI(x){
     }).then(function(response){
         console.log(response);
         $('#city').text(city);
-        $('#temperature').text(JSON.stringify(response.list[0].main.temp));
-        $('#humidity').text(JSON.stringify(response.list[0].main.humidity));
-        $('#wind-speed').text(JSON.stringify(response.list[0].wind.speed));
+        for(var i = 0; i < response.list.length; i++){
+            if (i === 0){
+                // Set current date using outputDate function
+                $('#date').text(outputDate(i));
 
-        var uvQuery = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + response.city.coord.lat +"&lon=" + response.city.coord.lon
-        getUV(uvQuery);
+                // Output current weather
+                $('#temperature').text(response.list[i].main.temp);
+                $('#humidity').text(response.list[i].main.humidity);
+                $('#wind-speed').text(response.list[i].wind.speed);
+                
+                // Get weather icon
+                var weatherimg = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png"
+                $('#weather-icon').attr("src", weatherimg);
+
+                // Get UV value using separate API
+                var uvQuery = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + response.city.coord.lat +"&lon=" + response.city.coord.lon
+                getUV(uvQuery);
+            } else {
+                // Ouput date, temperature and humidity to future weather
+                var newDiv = $('<div>').addClass('forecast');
+
+                // Date
+                var dateObj = $('<div>').text(outputDate(i));
+                newDiv.append(dateObj);
+
+                // Image
+
+                var weatherIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png")
+                newDiv.append(weatherIcon);
+
+                // Temperature
+                var temp = $('<div>').text("Temp: " + response.list[i].main.temp);
+                newDiv.append(temp);
+
+                // Humidity
+                var humidity = $('<div>').text("Humidity: " + response.list[i].main.humidity);
+                newDiv.append(humidity);
+
+                $('#forecast-output').append(newDiv);
+            }
+        }
+        
     })
 }
 
@@ -52,7 +105,7 @@ function getUV(x){
     })
 }
 function setStorage(){
-    localStorage.setItem("cityStorage", cityStorage);
+    localStorage.setItem("cityStorage", JSON.stringify(cityStorage));
 }
 
 function searchHistory(){
@@ -62,10 +115,6 @@ function searchHistory(){
         cityDiv.text(cityStorage[i]);
         $('#city-output').prepend(cityDiv);
     }
-}
-
-function getWeatherData(element, data){
-    
 }
 
 });
